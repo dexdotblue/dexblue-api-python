3c3775f980e0d60007b542eeb70bc57ac5c5ec17
Adrian Brennig <adrian@dex.blue>
2019-09-20 15:16:33 +0200

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dexblue-api",
-    version="0.2.1",
+    version="v0.1.0-beta1",
     author="dex.blue",
     author_email="tech@dex.blue",
     description="dex.blue API wrapper for python",
